8cf4cf3987d998b27d64a32f9041cc7f8449b0ca
kimste2 <steven_kim@student.uml.edu>
2017-07-18 15:45:27 -0400

    Hello World

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,11 @@
+// Classic Hello World program
+
+
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello world! My name is Steve. ");
+    }
+}
+
