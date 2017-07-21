5851d83e9696f978bb9758a66b4524b3f0688b01
kimste2 <steven_kim@student.uml.edu>
2017-07-21 11:49:55 -0400

    Update to random number

--- NumberGuessingGame/NumberGuessingGame.java
+++ NumberGuessingGame/NumberGuessingGame.java
@@ -1,21 +1,29 @@
 // number guessing game 
 import java.util.Scanner;
+import java.util.Random;
 public class NumberGuessingGame
 {
     public static void main(String[] args) 
     {
         int guess_counter;
-        int guesses[]=new int[10];
-     	int random_number = 42;  // TODO: make actual random number
+        int guesses[]= new int[10];
      	int guess = -1;
 
-     	Scanner reader = new Scanner(System.in);  // Reading from System.in
+     	Random randomGenerator = new Random();
+
+     	String continue_resonse; 
+
+     	Scanner reader = new Scanner(System.in);  
+        
+     	int random_number = randomGenerator.nextInt(100);
+
         System.out.println("Number Guessing Game!");
-        System.out.println("You have ten tries to guess the number between 0 and one hundred inclusive.  Good luck!");
+        System.out.println("You have ten tries to guess the number between zero and one hundred inclusive.  Good luck!");
 		
         for(guess_counter = 0; guess_counter <= 9; guess_counter++)
         {
         	System.out.print("Guess_counter is: " + (guess_counter + 1) +"\n");
+        	System.out.print("Random number is: " + random_number + "\n"); 
    			System.out.print("Enter a number: ");
         	guess = reader.nextInt();
         	if(guess == random_number)
@@ -33,13 +41,15 @@ public class NumberGuessingGame
       
 	      if(guess_counter > 9)
 	      {
-	      	  System.out.println("The correct number is 42. You guessed: ");
+	      	  System.out.print("The correct number is 42.\nYou guessed: ");
 	         for(guess_counter = 0; guess_counter <= 9; guess_counter++)
 	        {
 	        	System.out.print(guesses[guess_counter]);
 	        	System.out.print(", ");
 	        }
 	      }
+
+	      System.out.print("\nDo you want to play again?");
 
 	      System.out.println("\nThe End!");
 
